222f5561f4e180fa3455e07d6aacc02621ec91de
Chafiki <adamchafiki3@gmail.com>
2024-11-22 00:36:15 +0100

    init:bg-dot

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { motion } from "framer-motion"; // Adjusted import from "motion/react" to "framer-motion"
+// import svg from "./assets/angular.svg";
+
+const App = () => {
+  return (
+    <main className="min-h-screen flex justify-center items-center relative bg-dots">
+
+
+
+    </main>
+  );
+};
+
+export default App;
